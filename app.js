0c18834f5efcc9a862af545342e145eb900c38b5
João Soares <jsoares@infinera.com>
2020-11-16 19:35:19 +0000

    Telegram bot segregated from main project

---
+++ app.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const TeleBot = require('telebot');
+const environment_v = require('dotenv').config()
+
+const bot = new TeleBot(process.env.BOT_API_KEY);
+const MicroController = require('./models/microController');
+
+mongoose.connect(`mongodb://${process.env.DB_HOST}:27017/WaterMe`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log(`Connected to Database: mongodb://${process.env.DB_HOST}:27017/WaterMe`)
+})
+.catch(() => {
+  console.log(`Connection to Database failed: mongodb://${process.env.DB_HOST}:27017/WaterMe`)
+});
+
+/* Requirements:
+*  -> Has a user i expect the bot to delivery on demand sensor information
+*  -> Has a user i expect the bot to send a warning when sensors reach a certain threshold  
+*  -> Has a user i expect the bot to send certain flares of personality from time to time
+*/
+
+bot.on('/test', (msg) => {
+    return msg.reply.text('Sanity test')
+})
+
+
+// Sends latest registred Temperature for all the Temperature sensors
+bot.on('/temperature', (msg) => {
+
+    MicroController.findOne()
+    .then(controller => {
+        for (const sensor of controller.sensors) {
+            if (sensor.type.includes('temp'))
+            {
+                let reading_time = Date(sensor.readings[sensor.readings.length-1].time);
+                let reading_value = sensor.readings[sensor.readings.length-1].value
+                bot.sendMessage(msg.from.id,`${reading_time} : ${sensor.type} -> ${reading_value}`, { replyToMessage: msg.message_id });
+            }
+        }
+    })
+    .catch(err => {
+        console.log(err);
+    })
+})
+
+
+bot.start();
